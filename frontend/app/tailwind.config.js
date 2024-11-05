d1f7a4fc3cd721a1184315461373d9d986566048
Yogesh <yogesh@webinfomart.com>
2024-11-05 14:59:23 +0530

    add color theme

--- frontend/app/tailwind.config.js
+++ frontend/app/tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cloudy: "#b4ada6",
+        slateGray: "#778b9c",
+        nevada: "#65727e",
+        baliHai: "#90a3b1",
+        blueBayoux: "#4d6373",
+        bunker: "#080a0f",
+        oxfordBlue: "#3a4c5b",
+        dune: "#3b3735",
+        blueDianne: "#1f3648",
+        lemonGrass: "#989c8c",
+      },
+    },
   },
   plugins: [],
-}
-
+};
